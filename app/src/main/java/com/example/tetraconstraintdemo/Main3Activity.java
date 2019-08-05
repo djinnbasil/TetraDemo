10cd955bec502f7f3987b4553b4635045b001217
macbook <200411299@student.georgianc.on.ca>
2019-08-05 11:46:40 -0400

    Fourth Commit

--- app/src/main/java/com/example/tetraconstraintdemo/Main3Activity.java
+++ app/src/main/java/com/example/tetraconstraintdemo/Main3Activity.java
@@ -1,9 +1,14 @@
 package com.example.tetraconstraintdemo;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 import com.google.android.gms.common.api.Status;
@@ -19,20 +24,33 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main3Activity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     PlaceAutocompleteFragment placeAutoComplete;
+    TextView txtView ;
+    EditText locationstart;
+    EditText locationend1;
+
+    int AUTOCOMPLETE_REQUEST_CODE = 1;
     private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        txtView = findViewById(R.id.test);
+        locationstart = findViewById(R.id.locationstart);
+        locationend1 = findViewById(R.id.locationend);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -44,15 +62,53 @@ public class Main3Activity extends FragmentActivity implements OnMapReadyCallbac
 // Create a new Places client instance
         PlacesClient placesClient = Places.createClient(this);
 
-        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+
+        locationstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
+// Start the autocomplete intent.
+                Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields).build(Main3Activity.this);
+                startActivityForResult(intent, 2);
+
+
+
+
+            }
+
+
+
+
+
+        });
+
+    locationend1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
+// Start the autocomplete intent.
+            Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields).build(Main3Activity.this);
+            startActivityForResult(intent, 3);
+
+        }
+    });
+
+
+
+       /* AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
                 getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME,Place.Field.LAT_LNG));*/
 
-        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
-
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+       /* autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(@NonNull Place place) {
+                place.getLatLng();
                 Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                txtView.setText(place.getName()+"\n"+place.getId()+"\n"+ place.getLatLng());
+                locationstart.setText(place.getName());
+
+
 
             }
 
@@ -61,7 +117,7 @@ public class Main3Activity extends FragmentActivity implements OnMapReadyCallbac
                 Log.i(TAG, "An error occurred: " + status);
 
             }
-        });
+        });*/
 
 
     }
@@ -76,6 +132,28 @@ public class Main3Activity extends FragmentActivity implements OnMapReadyCallbac
     }
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+            if (resultCode == RESULT_OK) {
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                if (requestCode==2){
+                    locationstart.setText(place.getName());
+                }
+                if (requestCode==3){
+                    locationend1.setText(place.getName());
+                }
+
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                // TODO: Handle the error.
+                Status status = Autocomplete.getStatusFromIntent(data);
+                Log.i(TAG, status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+
+    }
 
 
 /*Also add googleMap != null contition in your click listener.
